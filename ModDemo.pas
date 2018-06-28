2008a0b1d087c7c827ef153dd36a22811292cc19
alex-gre <40606584+alex-gre@users.noreply.github.com>
2018-06-28 10:00:30 +0300

    create ModDemo.pas

---
+++ ModDemo.pas
@@ -0,0 +1,17 @@
+﻿//вычисление высокостного года
+program Year;
+var
+  Y: integer;
+  Days: integer;
+begin
+  
+  writeln('enter Year');
+ readln(Y);
+  if (Y mod 4 = 0) and (Y mod 100 > 0) or (Y mod 400 = 0)
+    then 
+      Days := 366
+    else
+      Days := 365;
+   writeln('Days ',Days);
+   readln
+end.
