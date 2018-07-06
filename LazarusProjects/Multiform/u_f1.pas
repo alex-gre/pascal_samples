bb12ed041ea9a3a8aa0438cf0233a4d3e1d7cd7b
alex-gre <40606584+alex-gre@users.noreply.github.com>
2018-07-06 21:19:18 +0300

    MultiForm demo method Show, ShowModal

---
+++ LazarusProjects/Multiform/u_f1.pas
@@ -0,0 +1,30 @@
+unit u_f1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Label1: TLabel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
