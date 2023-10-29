91e4849aa0eebbf61aa7e8b7ec343c1b27cf2821
alex-gre <40606584+alex-gre@users.noreply.github.com>
2023-10-29 14:20:42 +0300

    add FastDev project
    FastDev project this notepad for admin

---
+++ FastDev/script/bool_swap.py
@@ -0,0 +1,10 @@
+a=3
+b=45
+print("Begin values a,b =",a,b)
+a=a^b
+b=b^a
+a=a^b
+print("End boolean swap values a,b = ",a,b)
+
+
+
---
+++ FastDev/script/test2.py
@@ -0,0 +1,7 @@
+list = [1,4,5,6,10,-12]
+def test():
+  for i  in range(1,10):
+      if i == 5:
+         list.reverse()
+      print(list)
+test()
