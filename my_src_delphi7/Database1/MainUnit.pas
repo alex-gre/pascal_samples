ed2cdfd85b95fe64d423624b92b3aabe74a6a2d4
alex-gre <40606584+alex-gre@users.noreply.github.com>
2023-10-30 09:43:26 +0300

    Add files via upload

---
+++ my_src_delphi7/Database1/MainUnit.pas
@@ -0,0 +1,28 @@
+unit MainUnit;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, DB, ADODB, Grids, DBGrids;
+
+type
+  TForm1 = class(TForm)
+    ADOConnection1: TADOConnection;
+    BookTable: TADOTable;
+    DataSource1: TDataSource;
+    DBGrid1: TDBGrid;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
